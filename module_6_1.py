c51ae7db440b1812083351efceb707ddfefe6b14
alex777907 <garkovskij@bk.ru>
2024-11-01 14:57:57 +0300

    Update module_6_1.py

--- module_6_1.py
+++ module_6_1.py
@@ -1,53 +1,52 @@
-class Animal:
-    def __init__(self, name):
-        self.alive = True  # Живое
-        self.fed = False   # Накормленное
-        self.name = name   # Имя животного
+class Animal:#класс родитель для зверушек
+    alive = True
+    fed = False
 
-class Plant:
-    def __init__(self, name):
-        self.edible = False  # Съедобность
-        self.name = name      # Имя растения
-
-class Mammal(Animal):
     def eat(self, food):
-        if food.edible:
+        if food.edible == True:
             print(f"{self.name} съел {food.name}")
             self.fed = True
         else:
             print(f"{self.name} не стал есть {food.name}")
             self.alive = False
 
-class Predator(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
 
-class Flower(Plant):
-    pass  # Цветы несъедобные, ничего не переопределяем
+class Plant:#клас родитель для растений
+    edible = False
 
-class Fruit(Plant):
     def __init__(self, name):
-        super().__init__(name)
-        self.edible = True  # Плоды съедобные
+        self.name = name
 
-# Создание объектов классов
+
+class Mammal(Animal):#млекопитающие-класс наследник зверушек
+    def __init__(self, name):
+        self.name = name
+
+
+class Predator(Animal):#хищники-класс наследник зверушек
+    def __init__(self, name):
+        self.name = name
+
+
+class Flower(Plant):#цветы-класс наследник растений
+    pass
+
+
+class Fruit(Plant):#фрукты-класс наследний растений
+    edible = True
+
+
 a1 = Predator('Волк с Уолл-Стрит')
 a2 = Mammal('Хатико')
 p1 = Flower('Цветик семицветик')
 p2 = Fruit('Заводной апельсин')
 
-# Пример работы программы
 print(a1.name)
 print(p1.name)
 
 print(a1.alive)
 print(a2.fed)
-a1.eat(p1)  # Хищник пытается съесть несъедобный цветок
-a2.eat(p2)  # Млекопитающее ест съедобный фрукт
+a1.eat(p1)
+a2.eat(p2)
 print(a1.alive)
 print(a2.fed)
