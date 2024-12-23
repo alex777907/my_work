98648dfae1565b34d770801ebfe97fecb8fc14a9
alex777907 <garkovskij@bk.ru>
2024-12-23 07:59:11 +0300

    Update module_10_2.py

--- module_10_2.py
+++ module_10_2.py
@@ -7,40 +7,31 @@ class Knight(threading.Thread):
         super().__init__()
         self.name = name
         self.power = power
-        self.days_passed = 0
+        self.enemies = 100
+        self.days = 0
 
     def run(self):
-        global enemies_left
-
         print(f"{self.name}, на нас напали!")
+        while self.enemies > 0:
+            time.sleep(1)  # Задержка на 1 секунду
+            self.days += 1
+            self.enemies -= self.power
+            if self.enemies < 0:
+                self.enemies = 0
+            print(f"{self.name}, сражается {self.days} день(дня)..., осталось {self.enemies} воинов.")
 
-        while enemies_left > 0:
-            if enemies_left <= self.power:
-                enemies_left -= enemies_left
-            else:
-                enemies_left -= self.power
-
-            self.days_passed += 1
-            time.sleep(1)  # Пауза на одну секунду (один день)
-            print(f"{self.name} сражается {self.days_passed} день(дня)..., осталось {enemies_left} воинов.")
-
-        print(f"{self.name} одержал победу спустя {self.days_passed} дней(дня)!")
-
-    # Глобальная переменная для отслеживания количества врагов
+        print(f"{self.name} одержал победу спустя {self.days} дней(дня)!")
 
 
-enemies_left = 100
-
-# Создание и запуск двух рыцарей
+# Создание и запуск потоков
 first_knight = Knight('Sir Lancelot', 10)
 second_knight = Knight("Sir Galahad", 20)
 
 first_knight.start()
 second_knight.start()
 
-# Ожидание завершения потоков
+# Ожидание завершения обоих потоков
 first_knight.join()
 second_knight.join()
 
-# Сообщение об окончании боев
 print("Все битвы закончились!")
