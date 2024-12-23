e6089adaa34c3309c22b1c926a1a0e7229692bfa
alex777907 <garkovskij@bk.ru>
2024-12-23 08:03:25 +0300

    Update module_10_3.py

--- module_10_3.py
+++ module_10_3.py
@@ -1,52 +1,44 @@
-import random
 import threading
+import random
 import time
-
 
 class Bank:
     def __init__(self):
-        self.balance = 0
-        self.lock = threading.Lock()
+        self.balance = 0  # Изначальный баланс
+        self.lock = threading.Lock()  # Создание объекта Lock
 
     def deposit(self):
         for _ in range(100):
             amount = random.randint(50, 500)
-            with self.lock:
+            with self.lock:  # Блокировка для безопасного доступа к балансу
                 self.balance += amount
                 print(f"Пополнение: {amount}. Баланс: {self.balance}")
-
-                # Разблокируем замок, если баланс >= 500
-                if self.balance >= 500 and self.lock.locked():
-                    self.lock.release()
-
-            time.sleep(0.001)  # Задержка для имитации скорости выполнения пополнения
+            time.sleep(0.001)  # Задержка для имитации процесса
 
     def take(self):
         for _ in range(100):
             amount = random.randint(50, 500)
-            with self.lock:
-                print(f"Запрос на {amount}")
+            print(f"Запрос на {amount}")
+            with self.lock:  # Блокировка для безопасного доступа к балансу
                 if amount <= self.balance:
                     self.balance -= amount
                     print(f"Снятие: {amount}. Баланс: {self.balance}")
                 else:
                     print("Запрос отклонён, недостаточно средств")
-                    self.lock.acquire()  # Блокировка замка, если недостаточно средств
-
+            time.sleep(0.001)  # Задержка для имитации процесса
 
 # Создание объекта класса Bank
-bank = Bank()
+bk = Bank()
 
-# Создание и запуск потоков
-thread_deposit = threading.Thread(target=bank.deposit)
-thread_take = threading.Thread(target=bank.take)
+# Создание потоков для методов deposit и take
+th1 = threading.Thread(target=Bank.deposit, args=(bk,))
+th2 = threading.Thread(target=Bank.take, args=(bk,))
 
-thread_deposit.start()
-thread_take.start()
+# Запуск потоков
+th1.start()
+th2.start()
+th1.join()
+th2.join()
 
-# Ожидание завершения потоков
-thread_deposit.join()
-thread_take.join()
-
-# Итоговый баланс
-print(f"Итоговый баланс: {bank.balance}")
+# Вывод итогового баланса
+print(f'Итоговый баланс: {bk.balance}')
